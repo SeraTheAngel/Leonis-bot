0b8d1de362ab840379c15c66f4a5f405c43cea87
SeraTheAngel <jamilycordeiro009@gmail.com>
2026-03-26 16:47:46 -0300

    Adiciona o comando help.js na pasta src/commands/info/

---
+++ src/commands/info/help.js
@@ -0,0 +1,8 @@
+// Comando de ajuda
+module.exports = {  
+    name: 'help',  
+    description: 'Lista todos os comandos disponíveis.',  
+    execute(message) {  
+        // lógica do comando  
+    }  
+};
