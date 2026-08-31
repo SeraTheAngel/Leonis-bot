d503f0ca0437a34e42c5677859379fa314018272
SeraTheAngel <jamilycordeiro009@gmail.com>
2026-08-31 14:24:56 -0300

    feat: deploy administration and game commands

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -3,13 +3,10 @@ import { REST, Routes } from 'discord.js';
 import { actionCommand } from './commands/actions.js';
 import { utilityCommands } from './commands/utility.js';
 import { moderationCommands } from './commands/moderation.js';
-
-if (!process.env.DISCORD_TOKEN || !process.env.CLIENT_ID) {
-  throw new Error('Defina DISCORD_TOKEN e CLIENT_ID no .env');
-}
-
-const commands = [actionCommand, ...utilityCommands, ...moderationCommands].map(c => c.data.toJSON());
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
-
-await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+import { adminCommands } from './commands/admin.js';
+import { gameCommands } from './commands/games.js';
+if(!process.env.DISCORD_TOKEN||!process.env.CLIENT_ID)throw new Error('Defina DISCORD_TOKEN e CLIENT_ID no .env');
+const commands=[actionCommand,...utilityCommands,...moderationCommands,...adminCommands,...gameCommands].map(c=>c.data.toJSON());
+const rest=new REST({version:'10'}).setToken(process.env.DISCORD_TOKEN);
+await rest.put(Routes.applicationCommands(process.env.CLIENT_ID),{body:commands});
 console.log(`✅ ${commands.length} comandos registrados globalmente.`);
